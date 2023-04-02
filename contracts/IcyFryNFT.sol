4ec9064a44b879b4bbc108e3690b54a7a61c8ca5
Jonathan Le Brun <42697488+icyfry@users.noreply.github.com>
2023-04-02 21:20:52 +0200

    update contract

--- contracts/IcyFryNFT.sol
+++ contracts/IcyFryNFT.sol
@@ -3,16 +3,39 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract IcyFryNFT is ERC721, ERC721Burnable, Ownable {
+contract IcyFryNFT is ERC721,  ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("IcyFry", "ICY") {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://nft.icyfry.io/";
     }
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
 }
